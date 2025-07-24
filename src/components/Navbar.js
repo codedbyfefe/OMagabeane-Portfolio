1f0c18276f036b278b442b25b94dfe0e63e128d9
Omphile Ofentse Magabeane <91537564+Fefeswork@users.noreply.github.com>
2025-07-24 03:19:34 +0200

    minor changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <header className="flex justify-between items-center px-6 py-4 fixed w-full bg-white shadow-md z-50">
-      <h1 className="text-2xl font-bold tracking-wide">mirror</h1>
+      <h1 className="text-2xl font-bold tracking-wide">OM</h1>
       <nav className="space-x-6 text-sm font-medium tracking-wide">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
